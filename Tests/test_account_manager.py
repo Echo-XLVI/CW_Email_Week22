340b7c7aa213f2806e666e0e0a9c4374a202d4e3
Reza Rezaeemoghadam <rezarezaee469@gmail.com>
2024-05-25 18:04:55 +0330

    fixing some misunderstanding

--- Tests/test_account_manager.py
+++ Tests/test_account_manager.py
@@ -8,17 +8,22 @@ import os
 class TestLogin(unittest.TestCase):
 
 
-    @classmethod
-    def setUp(cls) -> None:        
-        cls.db_manager_obj = DataBaseConnection()
-        print(type(cls.db_manager_obj))
-        contact_manager_obj = ContactManager(cls.db_manager_obj)
-        cls.account_manager_obj = AccountManager(cls.db_manager_obj,contact_manager_obj)
+    def setUp(self) -> None:        
+        self.db_manager_obj = DataBaseConnection()
+        print(type(self.db_manager_obj))
+        contact_manager_obj = ContactManager(self.db_manager_obj)
+        self.account_manager_obj = AccountManager(self.db_manager_obj,contact_manager_obj)
         # aquierment  for display_contact_display
-        cls.main_acc = Accounts('reza@gmail.com','0912')
+        self.main_acc = Accounts('reza@gmail.com','0912')
         con_acc1 = Accounts('zahra@gmail.com','0915')
         con_acc2 = Accounts('arman@gmail.com','0925')
-        cls.main_acc.contacts_obj = [con_acc1,con_acc2]
+        self.main_acc.contacts_obj = [con_acc1,con_acc2]
+
+    def test_display(self):
+        # self.account_manager_obj.display_contact(self.main_acc)
+        # a  = os.popen('pwd').readlines()
+        # print(a)
+        self.assertTrue(True)
 
     @unittest.skip("skip shod")
     def test_login(self):
@@ -27,13 +32,7 @@ class TestLogin(unittest.TestCase):
         logged_in_obj = self.account_manager_obj.login(email)
         self.assertIsNotNone(logged_in_obj)
         self.assertEqual(email,logged_in_obj.email_address)
-    
-    def test_display(self):
-        # self.account_manager_obj.display_contact(self.main_acc)
-        # a  = os.popen('pwd').readlines()
-        # print(a)
-        self.assertTrue(True)
-    
-    @classmethod
-    def tearDown(cls) -> None:
-        cls.db_manager_obj.close_connection()
+
+
+    def tearDown(self) -> None:
+        self.db_manager_obj.close_connection()
